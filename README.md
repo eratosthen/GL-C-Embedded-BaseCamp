ca02ade25633dcb208b8edc8f3e456335d1b470c
eratosthen <dmytro.holub@nure.ua>
2021-10-30 12:02:01 +0300

    added second task

--- README.md
+++ README.md
@@ -7,4 +7,10 @@ additional info could be found on [GoogleDrive](https://drive.google.com/drive/f
 Wokshop_1:
 implement custom blinking sequence with 4 leds
 
+Workshop_2:
+implement at least 3 blinking schemes for all 4 LEDs on the board
+2 buttons would be used to select scheme (next one, previous one).
+2 buttons would be used to select blinking speed (speed up, slow down).
+Button at the middle of cross should be used to turn on/off blinking.
+All buttons need to be processed by interrupts.
 
---
+++ Workshop_02/Core/Inc/main.h
@@ -0,0 +1,86 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define INCR_Pin GPIO_PIN_6
+#define INCR_GPIO_Port GPIOC
+#define INCR_EXTI_IRQn EXTI9_5_IRQn
+#define DECR_Pin GPIO_PIN_8
+#define DECR_GPIO_Port GPIOC
+#define DECR_EXTI_IRQn EXTI9_5_IRQn
+#define PREV_Pin GPIO_PIN_9
+#define PREV_GPIO_Port GPIOC
+#define PREV_EXTI_IRQn EXTI9_5_IRQn
+#define STOP_Pin GPIO_PIN_15
+#define STOP_GPIO_Port GPIOA
+#define STOP_EXTI_IRQn EXTI15_10_IRQn
+#define NEXT_Pin GPIO_PIN_11
+#define NEXT_GPIO_Port GPIOC
+#define NEXT_EXTI_IRQn EXTI15_10_IRQn
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
